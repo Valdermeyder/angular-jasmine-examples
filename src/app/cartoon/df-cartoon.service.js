b32abf1fb38c7434bc5718d0594b3abfc630bf79
Volodymyr Huzar <vguzar@i.ua>
2016-12-07 16:59:29 +0100

    Add http example

--- src/app/cartoon/df-cartoon.service.js
+++ src/app/cartoon/df-cartoon.service.js
@@ -2,13 +2,20 @@
     'use strict';
     angular.module('df.core').service('DfCartoonService', DfCartoonService);
 
-    DfCartoonService.$inject = ['DfCharacterService'];
+    DfCartoonService.$inject = ['DfCharacterService', '$http'];
 
-    function DfCartoonService(DfCharacterService) {
+    function DfCartoonService(DfCharacterService, $http) {
         this.getCharacterTitles = function (characters) {
             return characters.map(function (character) {
                 return DfCharacterService.getNameWithTitle(character);
             }).join(', ');
-        }
+        };
+
+        this.getCharacters = function () {
+            return $http({
+                method: 'GET',
+                url: '/characters'
+            })
+        };
     }
 })();
--- src/app/cartoon/df-cartoon.service.spec.js
+++ src/app/cartoon/df-cartoon.service.spec.js
@@ -1,7 +1,9 @@
 'use strict';
 describe('DfCartoonService', function () {
     var DfCartoonService,
-        DfCharacterServiceSpy;
+        DfCharacterServiceSpy,
+        $httpBackend,
+        alladinCharacter, jasmineCharacter;
 
     beforeEach(function () {
         module('df.core');
@@ -13,17 +15,13 @@ describe('DfCartoonService', function () {
         inject(function ($injector) {
             DfCartoonService = $injector.get('DfCartoonService');
             DfCharacterServiceSpy = $injector.get('DfCharacterService');
+            $httpBackend = $injector.get('$httpBackend');
         });
+        alladinCharacter = {name: 'Alladin', title: "Prince"};
+        jasmineCharacter = {name: 'Jasmine', title: "Princess"};
     });
 
     describe('#getCharacterTitles', function () {
-        var alladinCharacter, jasmineCharacter;
-
-        beforeEach(function () {
-            alladinCharacter = {name: 'Alladin', title: "Prince"};
-            jasmineCharacter = {name: 'Jasmine', title: "Princess"};
-        });
-
         it('should get name with title for each cartoon character', function () {
             DfCartoonService.getCharacterTitles([alladinCharacter, jasmineCharacter]);
             expect(DfCharacterServiceSpy.getNameWithTitle).toHaveBeenCalledWith(alladinCharacter);
@@ -34,6 +32,47 @@ describe('DfCartoonService', function () {
             DfCharacterServiceSpy.getNameWithTitle.and.returnValues('Prince Alladin', 'Princess Jasmine');
             expect(DfCartoonService.getCharacterTitles([alladinCharacter, jasmineCharacter]))
                 .toBe('Prince Alladin, Princess Jasmine');
+        });
+    });
+
+    describe('#getCharacters', function () {
+        var getCharactersRequest;
+
+        beforeEach(function () {
+            getCharactersRequest = $httpBackend.expect('GET', '/characters');
+        });
+
+        afterEach(function() {
+            $httpBackend.verifyNoOutstandingExpectation();
+            $httpBackend.verifyNoOutstandingRequest();
+        });
+
+        it('should request characters from server', function () {
+            getCharactersRequest.respond(200);
+            DfCartoonService.getCharacters();
+            $httpBackend.flush();
+        });
+
+        it('should get character from server', function () {
+            var characters;
+            var serverCharacters = [alladinCharacter, jasmineCharacter];
+            getCharactersRequest.respond(200, serverCharacters);
+            DfCartoonService.getCharacters().then(function (response) {
+                characters = response.data;
+            });
+            $httpBackend.flush();
+            expect(characters).toEqual(serverCharacters);
+        });
+
+        it('should get error when server is not reachable', function () {
+            var error;
+            var errorMessage = 'Not Found';
+            getCharactersRequest.respond(404, errorMessage);
+            DfCartoonService.getCharacters().catch(function (response) {
+                error = response.data;
+            });
+            $httpBackend.flush();
+            expect(error).toEqual(errorMessage);
         });
     });
 });
