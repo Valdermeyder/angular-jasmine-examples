fdac0c32d54f3d8e64444aeb770acd990eaac69b
Volodymyr Huzar <vguzar@i.ua>
2016-12-16 16:58:17 +0100

    Use locals in test for cartoon controller

--- src/app/cartoon/df-cartoon.controller.spec.js
+++ src/app/cartoon/df-cartoon.controller.spec.js
@@ -1,30 +1,30 @@
 'use strict';
 describe('DfCartoonController', function () {
-    var dfCartoonCtrl, $controller, DfCharacterServiceSpy;
+    var dfCartoonCtrl, $controller;
 
     beforeEach(function () {
         module('df.core');
-        module(function ($provide) {
-            $provide.service('DfCharacterService', function () {
-                this.getBestCartoons = jasmine.createSpy('getBestCartoons');
-            });
-        });
         inject(function ($injector) {
             $controller = $injector.get('$controller');
-            DfCharacterServiceSpy = $injector.get('DfCharacterService');
         });
     });
 
     describe('#bestCartoons', function () {
+        var DfCharacterServiceSpy;
+
+        beforeEach(function () {
+            DfCharacterServiceSpy = jasmine.createSpyObj('DfCharacterService', ['getBestCartoons'])
+        });
+
         it('should get top 10 cartoons from service', function () {
-            dfCartoonCtrl = $controller('DfCartoonController');
+            dfCartoonCtrl = $controller('DfCartoonController', {DfCharacterService: DfCharacterServiceSpy});
             expect(DfCharacterServiceSpy.getBestCartoons).toHaveBeenCalledWith(10);
         });
 
         it('should be init with received from service value', function () {
             var bestCartoons = ['Alladin'];
             DfCharacterServiceSpy.getBestCartoons.and.returnValue(bestCartoons);
-            dfCartoonCtrl = $controller('DfCartoonController');
+            dfCartoonCtrl = $controller('DfCartoonController', {DfCharacterService: DfCharacterServiceSpy});
             expect(dfCartoonCtrl.bestCartoons).toEqual(bestCartoons);
         });
     });
